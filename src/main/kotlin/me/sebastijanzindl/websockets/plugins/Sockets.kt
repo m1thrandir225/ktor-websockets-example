cdde09d6609ac1c559de65fbb454c3b4467ee733
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-03-04 21:17:32 +0100

    feat: multiple connections + user_id's shown

--- src/main/kotlin/me/sebastijanzindl/websockets/plugins/Sockets.kt
+++ src/main/kotlin/me/sebastijanzindl/websockets/plugins/Sockets.kt
@@ -1,10 +1,13 @@
 package me.sebastijanzindl.websockets.plugins
 
+import io.ktor.network.sockets.*
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
 import java.time.Duration
+import java.util.*
+import me.sebastijanzindl.websockets.Connection
 
 fun Application.configureSockets() {
     install(WebSockets) {
@@ -14,12 +17,28 @@ fun Application.configureSockets() {
         masking = false
     }
     routing {
+        val connections = Collections.synchronizedSet<Connection?>(LinkedHashSet())
        webSocket("/chat") {
-           send("You are connected!")
-           for (frame in incoming) {
-               frame as? Frame.Text ?: continue
-               val receivedText = frame.readText()
-               send("You said: $receivedText")
+           println("Adding user");
+
+           val thisConn = Connection(this);
+           connections += thisConn;
+           try {
+               send("You are connected! There are ${connections.count()} users here.")
+               for(frame in incoming) {
+                   frame as? Frame.Text ?: continue;
+                   val receivedText = frame.readText()
+
+                   val textWithUsername = "[${thisConn.name}]: $receivedText"
+                   connections.forEach {
+                       it.session.send(textWithUsername)
+                   }
+               }
+           } catch (e: Exception) {
+               println(e.localizedMessage);
+           } finally {
+               println("Removing $thisConn")
+               connections -= thisConn;
            }
        }
     }
